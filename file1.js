a69dbcc7429975910347370bc3145664ec57c59d
Mahak-Agrawal2303 <mahakkagrawal123@gmail.com>
2024-08-12 11:38:54 +0530

    Add files via upload

---
+++ file1.js
@@ -0,0 +1,52 @@
+// let a=10;
+// document.write(a, "<br/>")
+// let b=5;
+// document.write(b)
+// let sum = a+b;
+// document.write(sum, "<br/>")
+// let difference = a-b;
+// document.write(difference, "<br/>")
+
+// let product = a*b;
+// document.write(product, "<br/>")
+// let quotient = a/b;
+// document.write(quotient, "<br/>")
+// let remainder = a%b;
+// document.write(remainder, "<br/>")
+// let isEqual = (a==b);
+// document.write(isEqual, "<br/>")
+// let isNotEqual = (a!=b);
+// document.write(isNotEqual, "<br/>")
+// let isGreater = (a>b);
+// document.write(isGreater, "<br/>")
+// let logicalAnd = (a>b && b<10);
+// document.write(logicalAnd, "<br/>")
+
+
+// let fname = "Mahak";
+// document.write("Hello" , fname, "<br>")
+// let age = 19;
+// document.write('Age', age, "<br>")
+// let isStudent = true;
+// document.write(`Student - ${isStudent? "yes" : "no"} <br>`)
+// let emptyValue = null;
+// document.write(`About us ${emptyValue || "Write Something"} <br>`)
+// let unintialized;
+// document.write(!unintialized && "Fill detail completely")
+
+// --------------
+
+
+
+let age = 16;
+if(age>17){
+    document.write("Eligible for vote")
+} else {
+    document.write("Not eligible for vote")
+}
+document.write("<br>")
+
+
+// or 
+
+document.write(age>17 ? "Eligible for vote": "Not eligible for vote")
